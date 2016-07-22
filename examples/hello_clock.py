e2875608b4ecf003f918d9aa4185d11de7061576
Hugo Herter <git@hugoherter.com>
2016-07-22 12:25:23 +0200

    Add hello_clock example (#1)

---
+++ examples/hello_clock.py
@@ -0,0 +1,23 @@
+import asyncio
+
+
+async def print_every_second():
+    "Print seconds"
+    while True:
+        for i in range(60):
+            print(i, 's')
+            await asyncio.sleep(1 * 0.1)
+
+
+async def print_every_minute():
+    for i in range(1, 10):
+        await asyncio.sleep(60 * 0.1)
+        print(i, 'minute')
+
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(
+    asyncio.gather(print_every_second(),
+                   print_every_minute())
+)
+loop.close()
