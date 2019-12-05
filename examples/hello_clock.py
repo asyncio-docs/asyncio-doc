880e108ec470b163a3f783133ab6193bf06c574a
Hugo Herter <git@hugoherter.com>
2019-12-05 13:13:22 +0100

    Fix issues in hello_clock.py (#29)
    Closes issue #28 by making both coroutines exit after ~10 minutes.

--- examples/hello_clock.py
+++ examples/hello_clock.py
@@ -1,23 +1,24 @@
 import asyncio
 
 
+async def print_every_minute():
+    "Print minutes for ten minutes"
+    for i in range(10):
+        await asyncio.sleep(60)
+        print(i, 'minute')
+
+
 async def print_every_second():
-    """Print seconds"""
-    while True:
+    "Print seconds for ten minutes"
+    for i in range(10 * 60):
         for i in range(60):
             print(i, 's')
             await asyncio.sleep(1)
 
 
-async def print_every_minute():
-    for i in range(1, 10):
-        await asyncio.sleep(60)
-        print(i, 'minute')
-
-
 loop = asyncio.get_event_loop()
 loop.run_until_complete(
-    asyncio.gather(print_every_second(),
-                   print_every_minute())
+    asyncio.gather(print_every_minute(),
+                   print_every_second())
 )
 loop.close()
